e0c99b1b1da57e113029a80629883ab1b909fba4
Fede <Fede@LAPTOP-GV4FMNVA>
2018-08-09 23:48:50 +0200

    watcher new files

--- Watcher/Program.cs
+++ Watcher/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security.Permissions;
 
-namespace Watcher
+public class Watcher
 {
-    class Program
+
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Run();
     }
+
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    public static void Run()
+    {
+        var P = @"C:\test";
+
+        // Create a new FileSystemWatcher and set its properties.
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = P;
+
+        /* Watch for changes in LastAccess and LastWrite times, and the renaming of files or directories. */
+        watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+        // Only watch text files.
+        watcher.Filter = "*.txt";
+
+        // Add event handlers.
+        watcher.Created += new FileSystemEventHandler(OnChanged);
+
+        // Begin watching.
+        watcher.EnableRaisingEvents = true;
+
+        // Wait for the user to quit the program.
+        Console.WriteLine("Press \'q\' to quit the sample.");
+        while (Console.Read() != 'q') ;
+    }
+
+
+
+    // Define the event handlers.
+    private static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        // Specify what is done when a file is changed, created, or deleted.
+        Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+    }
+
 }
+
+
+////Ref: https://msdn.microsoft.com/en-us/library/system.io.filesystemwatcher(v=vs.110).aspx
